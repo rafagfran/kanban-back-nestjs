28a501f2cc64b6e9de8223f82e7fb6d4940d8432
Rafael Gonçalves <rafaelgfra@hotmail.com>
2025-02-13 15:38:10 -0300

    feat: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
+
+@Module({
+	imports: [TaskModule],
+	controllers: [TaskController],
+	providers: [TaskService],
+})
+export class AppModule {}
